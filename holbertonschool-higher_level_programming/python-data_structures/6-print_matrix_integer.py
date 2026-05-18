27a5d06100a4043eca445aea031d57de81c920de
atanih <torresalva09@gmail.com>
2026-05-18 16:28:18 -0400

    fix order

--- holbertonschool-higher_level_programming/python-data_structures/6-print_matrix_integer.py
+++
@@ -1,5 +0,0 @@
-[5:05 PM, 5/17/2026] AJTorres:     return new_string
-[5:18 PM, 5/17/2026] AJTorres: #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join(["{:d}".format(num) for num in row]))
--- holbertonschool-higher_level_programming/python-data_structures/README.md
+++
@@ -1,6 +0,0 @@
-# Python - Data Structures: Lists, Tuples
-
-This project covers Python lists, tuples, and sequences.
-
-## Files
-- 0-print_list_integer.py: Function that prints all integers of a list.
